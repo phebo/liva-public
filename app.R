3fd6ba4e80efbd209edf0950f71ee33535a5785f
Phebo Wibbens <phebo.wibbens@insead.edu>
2019-09-18 15:21:52 +0200

    Add Shiny web app and script to build the LIVA

---
+++ app.R
@@ -0,0 +1,121 @@
+# "Shiny" web app as published on www.liva-measure.com
+# This app needs the Global LIVA database as input in the file db-liva.csv
+# This file can be downloaded from WRDS (Wharton Research Data Services), or generated using the script build-liva-dbase.R
+# It also needs the db-gics.csv and db-countries.csv files that are included in this repository
+
+# Copyright (C) 2019, Phebo Wibbens and Nicolaj Siggelkow
+  
+#   This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+library(shiny)
+library(tidyverse)
+
+df <- read_csv("db-liva.csv")
+dfGics <- read_csv("db-gics.csv")
+dfLoc <- read_csv("db-countries.csv")
+
+df <- left_join(df %>% mutate(gics4d = gsubind %/% 1e4),
+                 dfGics %>% select(gics4d, sector))
+df <- left_join(df, dfLoc)
+df <- df %>% mutate(sector = ifelse(is.na(gics4d), "Not recorded", sector)) %>%
+  mutate(gics4d = ifelse(is.na(gics4d), 9999, gics4d)) %>%
+  mutate(sector = ifelse(gics4d == 2540, "Media & Entertainment", sector)) %>%   # GICS 2540 ("Media") has been re-classified
+  mutate(sector = ifelse(gics4d == 4040, "Real Estate", sector)) %>%   # GICS 2540 ("Media") has been re-classified
+  mutate(country = ifelse(is.na(country), "Not recorded", country))
+
+sectors <- c("All", df %>% group_by(sector) %>% summarize(gics4d = last(gics4d)) %>% arrange(gics4d) %>% pull(sector))
+countries <- c("All", df %>% group_by(country) %>% summarize(mc = sum(mcend)) %>% arrange(-mc) %>% pull(country))
+years <- with(df, c(min(year), max(year)))
+
+convString <- function(vec, n=30) {
+  # Make a vector of strings lower case except for first character, and truncate to first n characters
+  paste0(substr(vec, 1, 1),
+         tolower(substr(vec, 2, n)))
+}
+
+ui <- fluidPage(
+  titlePanel("Top and bottom performers in the global LIVA database"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("years", "Total over all years from / to", min = years[1], max = years[2], value = years, sep=""),
+      selectInput("country", "Country", countries),
+      selectInput("sector", "Sector", sectors),
+      radioButtons("show", "Show", c("Companies", "Countries", "Sectors")),
+      checkboxInput("table", "Add table below chart"),
+      sliderInput("nco", "Max number of items to show", min = 10, max = 100, value = 40, step=10),
+      textInput("search", "Text search"),
+      p(tags$small(HTML("&copy 2019 Phebo Wibbens and Nicolaj Siggelkow")))
+    ),
+    mainPanel(plotOutput("plot", inline=T),
+              tableOutput("table"))
+  )
+)
+
+server <- function(input, output) {
+  newData <- reactive({
+    dfSub <- df %>% filter(year >= input$years[1], year <= input$years[2])
+    if(input$sector != "All") dfSub <- dfSub %>% filter(sector == input$sector)
+    if(input$country != "All") dfSub <- dfSub %>% filter(country == input$country)
+    dfSum <- dfSub %>% group_by(gvkey) %>%
+      summarize(conm=last(conm), liva = sum(liva), first=first(year), last=last(year),
+                country=last(country), sector = last(sector))
+
+    if(input$show == "Countries") {
+      dfSum <- dfSum %>% group_by(name = country) %>% summarize(liva = sum(liva), nco = n())
+    } else if(input$show == "Sectors") {
+      dfSum <- dfSum %>% group_by(name = sector) %>% summarize(liva = sum(liva), nco = n())
+    } else {
+      dfSum <- dfSum %>% mutate(name = convString(conm)) %>% group_by(name) %>%
+        summarize(conm=last(conm), liva = sum(liva), first=first(first), last=last(last),
+                  country=last(country), sector = last(sector))
+          # This allows for multiple gvkeys with same name to be combined, e.g. SK Holdings Co Ltd (gvkeys 209610 and 293194)
+    }
+    if(input$search != "") dfSum <- dfSum %>% filter(grepl(input$search, name, ignore.case = TRUE))
+    if(nrow(dfSum) > input$nco) {
+      dfSum <- dfSum %>% filter(min_rank(liva) <= input$nco  %/% 2 | min_rank(-liva) <= input$nco %/% 2) %>%
+        mutate(tb = factor(ifelse(min_rank(-liva) <= input$nco  %/% 2, "Top", "Bottom"), levels = c("Top", "Bottom")))
+    }
+    dfSum
+  })
+  
+  output$plot <- renderPlot({
+    dfSum <- newData()
+    ggOut <- ggplot(dfSum, aes(x=reorder(name, liva), y=liva)) +
+      geom_bar(stat="identity") +
+      coord_flip() + ylab('LIVA ($B)') + xlab("") + scale_y_continuous(position = "right") +
+      theme(text = element_text(size=18))
+    if("tb" %in% names(dfSum)) ggOut <- ggOut + facet_grid(tb ~ ., scales = "free_y")
+    ggOut
+  }, height = function() {dfSum <- newData(); 150 + 15 * nrow(dfSum)},
+  width = 550)
+  
+  output$table <- renderTable({
+    if(input$table) {
+      dfSum <- newData()
+      if(input$show == "Countries") {
+        dfSum %>% arrange(-liva) %>% select(`Country` = name, `LIVA ($B)` = liva, `Number of companies` = nco)
+      } else if(input$show == "Sectors") {
+        dfSum %>% arrange(-liva) %>% select(`Sector` = name, `LIVA ($B)` = liva, `Number of companies` = nco)
+      } else {
+        dfSum %>% arrange(-liva) %>% mutate(first = as.integer(first), last = as.integer(last)) %>%
+          select(`Company name` = name, `LIVA ($B)` = liva, Country = country, Sector = sector, From = first, To = last)
+      }
+    } else {
+      NULL
+    }
+  }, digits = 1)
+}
+
+shinyApp(ui = ui, server = server)
---
+++ build-liva-dbase.R
@@ -0,0 +1,133 @@
+# This script generates the global LIVA database and can be adapted to create a custom LIVA database
+# The following input files need to be supplied, e.g. from WRDS (Wharton Research Data Services):
+# - db-compustat-na-security.csv: Compustat NA Security database (monthly)
+# - db-compustat-global-security.csv: Compustat Global Security database (daily, with end-of-month data only)
+# - db-G_EXRT_DLY.csv: Compustat daily exchange rate database
+# Please see the file db-compustat-variables.csv for an overview of the variables needed and selection criteria used
+# The file db-liva-variable-descriptions.csv contains an overview of the variables in the resulting database
+
+# Copyright (C) 2019, Phebo Wibbens and Nicolaj Siggelkow
+
+#   This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+library(tidyverse)
+
+minMc <- 0.1  # (in USD B) Include only companies wich have reached market cap of >$100M at any point in their history
+years <- c(1999, 2018)  # Both years inclusive
+baseYr <- 2018 # Base year for discounting LIVA (uses end-of-year); this changes all LIVAs by a constant factor
+excludeCompanies <- c("123916", "033625", "181283", "313077", "290168", "015520") # GVKEYs of companies with data integrity issues
+excludeCountries <- c("ZWE", "BRA", "VEN", "ARG") # Countries with data integrity issues (e.g. due to hyper-inflation)
+
+# Read files, adjust path names as necessary
+dfNA <- read_csv("db-compustat-na-security.csv", col_types = cols(prican = "c", prirow = "c"))  # Compustat NA security monthly
+dfGlobal <- read_csv("db-compustat-global-security.csv", col_types = cols(prican = "c")) # Compustat Global security daily
+dfFx <- read_csv("db-G_EXRT_DLY.csv", col_types = cols(exrattpd = "c"))  # Compustat currency conversion daily
+
+# Make currency conversion to USD
+dfFx <- dfFx %>% filter(tocurd == "USD") %>% select(datadate, usd = exratd) %>%
+  full_join(dfFx, .) %>%
+  mutate(usdconv = exratd / usd) %>%
+  select(curcd = tocurd, datadate, usdconv) %>% arrange(curcd, datadate)
+
+# Combine and clean-up databases
+df <- bind_rows(dfNA %>% rename(curcd = curcdm, prcc = prccm, trf = trfm, csho = cshom, ajex = ajexm),
+                dfGlobal %>% rename(curcd = curcdd, prcc = prccd, trf = trfd, csho = cshoc, ajex = ajexdi))
+df <- inner_join(df, dfFx)
+df <- df %>%
+  filter(
+    !gvkey %in% excludeCompanies, 
+    !loc %in% excludeCountries, 
+    !is.na(prcc + ajex + trf),
+    pmin(prcc, ajex, trf) >= 0.01,  # To prevent rounding issues
+    datadate >= (years[1] - 1) * 1e4 + 1200,
+    datadate < (years[2] + 1) * 1e4) %>%
+  mutate(
+    prc = prcc / usdconv,
+    mcend = prc * csho / 1e9,
+    month = datadate %/% 100,
+    year = datadate %/% 1e4,
+    date = as.Date(as.character(datadate), format = "%Y%m%d"))
+dfCo <- df %>% group_by(gvkey) %>% summarize(mc=max(mcend)) %>% filter(mc >= minMc)
+df <- df %>%
+  filter(gvkey %in% dfCo$gvkey) %>%
+  arrange(gvkey, month, iid)
+df <- df %>%  distinct(gvkey, month, .keep_all = T)
+
+# Calculate TSR (for same issue in consecutive months only)
+df <- inner_join(df,
+                 df %>% group_by(month) %>% summarize() %>% arrange(month) %>% mutate(monthno = row_number() - 1))
+df <- df %>% group_by(gvkey, iid) %>% mutate(
+    cons = (monthno == lag(monthno) + 1),
+    mcbeg = lag(mcend),
+    tsr = (prc * trf / ajex) / lag(prc * trf / ajex) - 1
+  ) %>% ungroup()
+df <- df %>% filter(cons & !is.na(tsr + mcbeg) ) %>%
+  mutate(usdtsr = tsr * mcbeg)
+
+# Calculate market index and LIVA
+dfMkt <- df %>% group_by(month) %>%
+  summarize(
+    mcbeg = sum(mcbeg),
+    usdtsr = sum(usdtsr)) %>%
+  mutate(
+    mkttsr = usdtsr / mcbeg,
+    lmkttsr = log1p(mkttsr),
+    lmktex = cumsum(lmkttsr),
+    index = exp(lmktex),
+    mcindex = lead(mcbeg)/first(mcbeg),
+    delta = index[month == baseYr * 100 + 12] / index)
+df <- inner_join(df, dfMkt %>% select(month, mkttsr, delta))
+df <- df %>% mutate(
+  ler = log1p(tsr) - log1p(mkttsr),
+  liva = (tsr - mkttsr) * mcbeg * delta,
+  cf = (1 + tsr) * mcbeg - mcend,
+  livac = cf * delta,  # Cash contribution to LIVA
+  mcadj = mcbeg * delta
+)
+dfCoYr <- df %>% group_by(gvkey, year) %>% summarize(
+  conm = last(conm),
+  loc = last(loc),
+  gsubind = last(gsubind),
+  liva = sum(liva),
+  ler = sum(ler),
+  mcbeg = first(mcbeg),
+  mcend = last(mcend),
+  nmo = n()
+) %>% ungroup()
+stopifnot(all(abs((dfCoYr %>% group_by(year) %>% summarize(liva = sum(liva)))$liva) < 1e-10)) # LIVA for each month should be 0
+
+dfCo <- dfCoYr %>% group_by(gvkey) %>% summarize(
+  conm = last(conm),
+  loc = last(loc),
+  gsubind = last(gsubind),
+  liva = sum(liva),
+  ler = sum(ler),
+  mcbeg = first(mcbeg),
+  mcend = last(mcend),
+  begin = first(year),
+  end = last(year),
+  nmo = sum(nmo)
+) %>% mutate(
+  er = expm1(ler / (end - begin))
+) %>% arrange(-liva) %>%
+  mutate(
+    cumliva = cumsum(liva),
+    rank = row_number()
+    )
+
+# Write database to CSV file
+write_csv(dfCoYr, "db-liva.csv")
+
+
